6e463ac714762927e9eead2cbc57e8753a2e1bf8
wyx <1649038406@qq.com>
2020-09-20 10:02:50 +0800

    Initial Commit

---
+++ opencv/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv
+//
+//  Created by 王艺璇 on 2020/9/20.
+//  Copyright © 2020 王艺璇. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
